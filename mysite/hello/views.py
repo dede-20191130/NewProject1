82cc2abc427c5713490228b3f9bd6b31de9bd002
R.Ishikawa <a0077476@gmail.com>
2019-09-20 17:03:39 +0900

    first commit

---
+++ mysite/hello/views.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http.response import HttpResponse
+
+
+def hello_world(request):
+    return HttpResponse('Hello World!')
+
+
+def hello_template(request):
+    d = {
+        'hour': datetime.now().hour,
+        'minite': datetime.now().minute,
+        'message': '指定文字列表示',
+    }
+    return render(request, 'index.html', d)
+
+
+def hello_if(request):
+    d = {
+        'is_visible': True,
+        'empty_str': 'イヴェルカーナFROST',
+    }
+    return render(request, 'if.html', d)
+
+
+def hello_for(request):
+    d = {
+        'objects': range(10),
+    }
+    return render(request, 'for.html', d)
+
+
+def hello_get_query(request):
+    d = {
+        'your_name': request.GET.get('your_name')
+    }
+    return render(request, 'get_query.html', d)
