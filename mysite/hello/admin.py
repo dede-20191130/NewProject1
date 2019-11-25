b886bd917d7e0eaf9dcd562df76f69139a0430c4
dede2 <a0077476@gmail.com>
2019-11-26 01:40:44 +0900

    test

--- mysite/hello/admin.py
+++ mysite/hello/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Hello
 
-# Register your models here.
+
+class HelloAdmin(admin.ModelAdmin):
+    list_display = ('id', 'your_name')
+    # list_display = ('id', 'your_name', 'format_published_at')
+    #
+    # def format_published_at(self, obj):
+    #     if obj.published_at is not None:
+    #         return '{0:%Y年%m月%d日 %H時%M分}'.format(obj.published_at)
+    #
+    # format_published_at.short_description = '公開開始日時'
+
+
+admin.site.register(Hello, HelloAdmin)
